f269302079c737a974f2f810bf7fe5a8b4dffcce
Michael Ludwig <lhkbob@gmail.com>
2014-09-05 18:39:06 -0500

    Implement and document function and union types.

---
+++ src/main/java/com/lhkbob/fxsl/lang/FunctionType.java
@@ -0,0 +1,291 @@
+package com.lhkbob.fxsl.lang;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.lhkbob.fxsl.util.Preconditions.notNull;
+import static com.lhkbob.fxsl.util.Preconditions.validCollection;
+
+/**
+ * Function Types
+ * ==============
+ *
+ * FXSL is a functional language, and as such functions can be typed and referenced just like any other
+ * value. Like other languages a function consists of a list of parameters and an invoked body that may use
+ * those parameters to return some new value. A function type describes the ordered types within the parameter
+ * list and the return type of the function when it is invoked.
+ *
+ * Functions may be curried, making it possible to invoke a function with fewer than the total arguments.
+ * When this occurs a new function value is returned that takes the remaining parameters as its input and
+ * returns the original return type.  Once the full parameter list is completed the original function body is
+ * be invoked.
+ *
+ * FXSL has the related concept of a function union, described in more detail in {@link
+ * com.lhkbob.fxsl.lang.UnionType}. Function unions are a way to support ad hoc polymorphism that allows
+ * multiple function definitions to be referenced by the same symbol.
+ *
+ * ## Assignability and shared types
+ *
+ * A type can only be assigned to a function type value if that type is a wildcard or a function or union
+ * that meets specific criteria that depends on the target function type. If there are two types, `A` and `B`
+ * where `A` is a functional type, the behavior of assignment and type conversion will be explained below for
+ * when `B` is a wildcard, another function type, or a union.
+ *
+ * If `B` is a wildcard then it can be assigned to `A` by creating a constraint on the types `B` may be
+ * instantiated as when wildcards are removed. The conversion between `A` and `B` is `A`.
+ *
+ * If `B` is another function type then it can be assigned to `A` if its return type is assignable to the
+ * return type of `A` and every parameter type of `A` is assignable to the corresponding parameter of `B`.
+ * Essentially if `B` has wider parameter types and a narrower return type compared to `A` it can be assigned
+ * to `A`. It must also have the exact same number of parameters. Parameters are compared based on their index
+ * within the parameter list.
+ *
+ * The shared function type between two function types `A` and `B` is another function type. The new
+ * function type has a return type that is the shared type of `A`'s and `B`'s return types. If this doesn't
+ * exist the shared function type does not exist. The two function types must have the same number of
+ * parameters, and the parameter list is the narrowest parameter list. Specifically if all of `A`'s parameters
+ * are assignable to the corresponding ones in `B` then `A`'s list is used. If all of `B`'s parameters are
+ * assignable to their match in `A` then `B`'s list is used. If this relationship does not hold for all
+ * parameters then the shared function type does not exist.
+ *
+ * When `B` is a function union type, it may be assigned to `A` if any of its functional options are
+ * assignable to `A`. The shared type between a function type and a union is a new union made of all valid
+ * shared types between the function type and union options. If there are no valid shared types the overall
+ * shared type does not exist. If there is only a single valid shared option that function type is used as the
+ * shared type since a union must have at least two options.
+ *
+ * ## Concreteness
+ *
+ * A function type is concrete if all of its parameter types are concrete and its return type is concrete.
+ *
+ * @author Michael Ludwig
+ */
+public class FunctionType implements Type {
+    private static final double BASE_COST = 20.0;
+    private static final double PARAM_COST = 2.0;
+
+    private final List<Type> parameters;
+    private final Type returnType;
+
+    /**
+     * Create a new function type that takes the given parameter types (`parameters`) and will return the
+     * type specified by `returnType`. The parameter list is copied so modifications to `parameters` will not
+     * affect the created type.
+     *
+     * @param parameters The parameter list
+     * @param returnType The return type
+     * @throws java.lang.IllegalArgumentException if `parameters` is empty
+     * @throws java.lang.NullPointerException     if `parameters` is null, contains null elements, or if
+     *                                            `returnType` is null
+     */
+    public FunctionType(List<? extends Type> parameters, Type returnType) {
+        validCollection("parameters", parameters);
+        notNull("returnType", returnType);
+
+        this.parameters = Collections.unmodifiableList(new ArrayList<>(parameters));
+        this.returnType = returnType;
+    }
+
+    /**
+     * Get the return type of this function, e.g. the type of any invocations on instances of this type.
+     *
+     * @return The return type of the function
+     */
+    public Type getReturnType() {
+        return returnType;
+    }
+
+    /**
+     * Get all parameter types of this function, in the order they were declared in the shader. The list
+     * will have at least one type. The returned list cannot be modified.
+     *
+     * @return The parameter types of the function
+     */
+    public List<Type> getParameterTypes() {
+        return parameters;
+    }
+
+    /**
+     * Get the type of the parameter specified by `index`, which must be between `0` and
+     * `getParameterCount() - 1`. This is equivalent to getting the same index from the list returned by
+     * `getParameterTypes()`.
+     *
+     * @param index The parameter index to lookup
+     * @return The parameter type for `index`
+     * @throws java.lang.IndexOutOfBoundsException if `index` is outside the number of parameters
+     */
+    public Type getParameterType(int index) {
+        return parameters.get(index);
+    }
+
+    /**
+     * Get the maximum number of parameters instances of this function type can be invoked with. If invoked
+     * with fewer a curried function value is created instead of actually invoking the base function.
+     *
+     * @return The parameter count
+     */
+    public int getParameterCount() {
+        return parameters.size();
+    }
+
+    @Override
+    public double getTypeComplexity() {
+        double cost = BASE_COST + returnType.getTypeComplexity();
+        for (Type p: parameters) {
+            cost += PARAM_COST + p.getTypeComplexity();
+        }
+        return cost;
+    }
+
+    @Override
+    public double getAssignmentCost(Type t) {
+        if (!isAssignableFrom(t)) {
+            return Double.POSITIVE_INFINITY;
+        } else if (t instanceof WildcardType) {
+            return getTypeComplexity();
+        } else if (t instanceof UnionType) {
+            double cost = Double.POSITIVE_INFINITY;
+            for (Type o: ((UnionType) t).getOptions()) {
+                cost = Math.min(cost, getAssignmentCost(o));
+            }
+            return cost;
+        } else {
+            FunctionType other = (FunctionType) t;
+            double cost = returnType.getAssignmentCost(other.returnType);
+            for (int i = 0; i < parameters.size(); i++) {
+                cost += other.parameters.get(i).getAssignmentCost(parameters.get(i));
+            }
+            return cost;
+        }
+    }
+
+    @Override
+    public boolean isAssignableFrom(Type type) {
+        if (!(type instanceof FunctionType)) {
+            if (type instanceof UnionType) {
+                // if any of the options is assignable then the whole type is
+                for (Type o : ((UnionType) type).getOptions()) {
+                    if (isAssignableFrom(o)) {
+                        return true;
+                    }
+                }
+            }
+            // otherwise not a union, or union had no assignable function option
+            return type instanceof WildcardType;
+        }
+
+        FunctionType other = (FunctionType) type;
+        if (!returnType.isAssignableFrom(other.returnType)) {
+            // cannot convert the other function's result to this type
+            return false;
+        }
+        // must be able to convert all parameters of this function type to other
+        // (e.g. other is being invoked as a delegate)
+        if (parameters.size() != other.parameters.size()) {
+            return false;
+        }
+        for (int i = 0; i < parameters.size(); i++) {
+            if (!other.parameters.get(i).isAssignableFrom(parameters.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public Type getSharedType(Type type) {
+        if (!(type instanceof FunctionType)) {
+            // special case for unions of function types, use its implementation
+            if (type instanceof UnionType) {
+                return type.getSharedType(this);
+            } else if (type instanceof WildcardType) {
+                return this;
+            } else {
+                return null;
+            }
+        }
+        // A valid function conversion uses the converted return type, and the union/most-specific-subtype of each
+        // parameter type. However, that operation doesn't exist, so we enforce parameter type equality.
+        FunctionType other = (FunctionType) type;
+
+        Type convertedReturnType = returnType.getSharedType(other.returnType);
+        if (convertedReturnType == null) {
+            return null;
+        }
+
+        if (parameters.size() != other.parameters.size()) {
+            return null;
+        }
+
+        boolean thisAssignFromOther = true;
+        for (int i = 0; i < parameters.size(); i++) {
+            if (!parameters.get(i).isAssignableFrom(other.parameters.get(i))) {
+                // if other's parameter does not assign to this parameter then other's parameter list is
+                // not the narrowest to choose from
+                thisAssignFromOther = false;
+                break;
+            }
+        }
+
+        if (thisAssignFromOther) {
+            // other is the narrowest function parameter list
+            return new FunctionType(other.parameters, convertedReturnType);
+        } else {
+            for (int i = 0; i < parameters.size(); i++) {
+                if (!other.parameters.get(i).isAssignableFrom(parameters.get(i))) {
+                    // this parameter does not assign to other's so this parameter list is not the
+                    // narrowest to choose from, but neither is other so the conversion is invalid
+                    return null;
+                }
+            }
+            return new FunctionType(parameters, convertedReturnType);
+        }
+    }
+
+    @Override
+    public boolean isConcrete() {
+        if (!returnType.isConcrete()) {
+            return false;
+        }
+
+        for (Type p : parameters) {
+            if (!p.isConcrete()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object t) {
+        if (!(t instanceof FunctionType)) {
+            return false;
+        }
+        FunctionType f = (FunctionType) t;
+        return f.returnType.equals(returnType) && f.parameters.equals(parameters);
+    }
+
+    @Override
+    public int hashCode() {
+        return parameters.hashCode() ^ returnType.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('(');
+        boolean first = true;
+        for (Type p: parameters) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(", ");
+            }
+            sb.append(p.toString());
+        }
+        sb.append(" -> ").append(returnType.toString()).append(")");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/lhkbob/fxsl/lang/UnionType.java
@@ -0,0 +1,213 @@
+package com.lhkbob.fxsl.lang;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.lhkbob.fxsl.util.Preconditions.validCollection;
+
+/**
+ * Function Union Types
+ * ====================
+ *
+ * Function unions provide ad hoc polymorphism to FXSL. A union type is a set of types that can resolve to
+ * function types, namely actual function types, wildcards, and other union types. Defining a new union type
+ * based on other union types makes it easy to overload the common operators to support custom types. In
+ * general it should not be necessary to declare union types in FXSL but rely on implicit type inference;
+ * otherwise they can become quite unwieldy.
+ *
+ * ## Invoking function unions
+ *
+ * A function union can be invoked just like a function. However, only a single function within its set of
+ * options is actually invoked. The specific function is selected based on the types of the input arguments of
+ * the actual invocation. If no function option can be made to match then the union cannot be invoked in the
+ * requested manner and a compilation error is raised. Because FXSL has no type inheritance a heuristic based
+ * on edit distance is used to determine what the best function is to invoke.
+ *
+ * First only function options that have parameters which the argument values are assignable to are
+ * considered. Then the net assignment cost is computed for the arguments to each function. The function
+ * options are ordered by this cost and the minimal costing function is selected. If a function has more
+ * parameters than arguments provided (so the invocation creates a curried function), it is ordered after the
+ * other functions that have fewer remaining parameters. This makes currying within a union only possible when
+ * no other option matches with a smaller argument list.
+ *
+ * ## Assignability and shared types
+ *
+ * One union type can only be assigned to another union type if it contains an assignable option for each
+ * option in the second type. The shared type between two unions is a new union made of valid shared types
+ * between their options. The shared type between a union type and a function type is described in {@link
+ * com.lhkbob.fxsl.lang.FunctionType}. Note that a function type cannot be assigned to a union type, only the
+ * opposite direction is valid.
+ *
+ * Wildcards can still be assigned to unions, but impose a constraint upon the wildcard's instantiated type.
+ * Similarly the shared type between a union and a wildcard is the union.
+ *
+ * ## Concreteness
+ *
+ * A union type is concrete if all of its function type options are concrete. This is different from the
+ * expression concreteness of invoking a union, which is not concrete because a function must be selected to
+ * be a concrete expression.
+ *
+ * @author Michael Ludwig
+ */
+public class UnionType implements Type {
+    private static final double BASE_COST = 20.0;
+    private static final double OPTION_COST = 3.0;
+
+    private final Set<Type> functions;
+
+    /**
+     * Create a new union type that is the union of the given types. These types may be function types,
+     * wildcard types, or other union types. Any union types included within this set are expanded to their
+     * function options. If after this flattening occurs there are not two unique options the union type is
+     * invalid and an exception is thrown.
+     *
+     * @param functions The function type options making up the union
+     * @throws java.lang.IllegalArgumentException if `functions` references a type other than UnionType,
+     *                                            FunctionType, and WildcardType, or if the net option size is
+     *                                            less than 2.
+     * @throws java.lang.NullPointerException     if `functions` is null or contains null elements
+     */
+    public UnionType(Set<? extends Type> functions) {
+        validCollection("functions", functions);
+
+        Set<Type> flattened = new HashSet<>();
+        for (Type t : functions) {
+            if (!(t instanceof UnionType || t instanceof FunctionType || t instanceof WildcardType)) {
+                throw new IllegalArgumentException("Type union only supports function types, not: " + t);
+            }
+            if (t instanceof UnionType) {
+                flattened.addAll(((UnionType) t).getOptions());
+            } else {
+                flattened.add(t);
+            }
+        }
+
+        if (flattened.size() < 2) {
+            throw new IllegalArgumentException("Union requires at least two unique types");
+        }
+        this.functions = Collections.unmodifiableSet(flattened);
+    }
+
+    /**
+     * Get the set of function options represented by this union type. The returned set will only contain
+     * function types and wildcard types. It will have at least two elements. The returned set is immutable.
+     *
+     * @return The function options of this type
+     */
+    public Set<Type> getOptions() {
+        return functions;
+    }
+
+    @Override
+    public double getTypeComplexity() {
+        double complexity = BASE_COST;
+        for (Type o : functions) {
+            complexity += OPTION_COST + o.getTypeComplexity();
+        }
+        return complexity;
+    }
+
+    @Override
+    public double getAssignmentCost(Type t) {
+        if (!isAssignableFrom(t)) {
+            return Double.POSITIVE_INFINITY;
+        } else if (t instanceof WildcardType) {
+            return getTypeComplexity();
+        } else {
+            UnionType other = (UnionType) t;
+            double cost = 0.0;
+
+            for (Type f : functions) {
+                cost += f.getAssignmentCost(other);
+            }
+
+            return cost;
+        }
+    }
+
+    @Override
+    public boolean isAssignableFrom(Type t) {
+        if (!(t instanceof UnionType)) {
+            // NOTE: a FunctionType can't be assigned to a UnionType, all options must be provided to be assignable
+            return t instanceof WildcardType;
+        }
+
+        UnionType otherType = (UnionType) t;
+        // every one of this type's options must have an option in other that is assignable to it
+        for (Type f : functions) {
+            if (!f.isAssignableFrom(otherType)) {
+                // no function option in other could be used as f
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isConcrete() {
+        for (Type f : functions) {
+            if (!f.isConcrete()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Type getSharedType(Type t) {
+        if (t instanceof UnionType || t instanceof FunctionType) {
+            // the valid conversion is the cross product of both option sets, with only valid conversions;
+            // since getSharedType is commutative the product is simpler to compute
+            Set<Type> conversion = new HashSet<>();
+            for (Type f : functions) {
+                Type c = f.getSharedType(t);
+                if (c != null) {
+                    conversion.add(c);
+                }
+            }
+            if (conversion.isEmpty()) {
+                // no useful overlap
+                return null;
+            } else if (conversion.size() == 1) {
+                // single intersection
+                return conversion.iterator().next();
+            } else {
+                return new UnionType(conversion);
+            }
+        } else if (t instanceof WildcardType) {
+            return this;
+        } else {
+            // incompatible types
+            return null;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof UnionType)) {
+            return false;
+        }
+        return functions.equals(((UnionType) o).functions);
+    }
+
+    @Override
+    public int hashCode() {
+        return functions.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (Type o: functions) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(" | ");
+            }
+            sb.append(o.toString());
+        }
+        return sb.toString();
+    }
+}
